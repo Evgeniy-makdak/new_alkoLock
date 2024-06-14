5f4bf9a4055d01d7b65382206c92f9c28c02c24c
Evgeniy-makdak <makdak23@mail.ru>
2024-06-14 09:53:23 +0300

    Убрал из панели фильтров в событиях поиск по дате привязки, так как дублирует имеющийся фильтр по датам

--- src/features/attachments_filter_panel/ui/AttachmentsFilterPanel.tsx
+++ src/features/attachments_filter_panel/ui/AttachmentsFilterPanel.tsx
@@ -1,6 +1,5 @@
 import { AlcolockSelect } from '@entities/alcolock_select';
 import { CarsSelect } from '@entities/cars_select';
-import { DateOfLinkSelect } from '@entities/date_of_link_select';
 import { FilterPanel } from '@entities/filter_panel';
 import { UsersCreateAttachSelect } from '@entities/users_create_attach_select';
 import { UsersSelect } from '@entities/users_select';
@@ -58,17 +57,6 @@ export const AttachmentsFilterPanel = ({ open }: { open: boolean }) => {
                 .ATTACHMENTS_WIDGET_HEADER_FILTER_INPUT_ALCOLOKS
             }
             name="alcolocks"
-          />
-          <DateOfLinkSelect
-            multiple={true}
-            name="dateLink"
-            setValueStore={setFilters}
-            value={filters.dateLink}
-            label="Поиск по дате привязки"
-            testid={
-              testids.page_attachments.attachments_widget_header
-                .ATTACHMENTS_WIDGET_HEADER_FILTER_INPUT_DATE_LINK
-            }
           />
         </FilterPanel>
       ) : null}
