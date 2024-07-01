f6c1526aabd990147fec1d2911f664f9eceed71c
Evgeniy-makdak <makdak23@mail.ru>
2024-07-01 13:42:11 +0300

    Исправлена реализация вывода ошибки при создании ТС с существующим номером или vin

--- src/features/car_add_change_form/api/useCarAddChangeFormApi.ts
+++ src/features/car_add_change_form/api/useCarAddChangeFormApi.ts
@@ -6,22 +6,10 @@ import { StatusCode } from '@shared/const/statusCode';
 import { QueryKeys } from '@shared/const/storageKeys';
 import { useConfiguredQuery } from '@shared/hooks/useConfiguredQuery';
 import { useUpdateQueries } from '@shared/hooks/useUpdateQuerys';
-import type { ChangeCarBody, CreateCarBody, ID, IError } from '@shared/types/BaseQueryTypes';
+import type { ChangeCarBody, CreateCarBody, ID } from '@shared/types/BaseQueryTypes';
 import { useMutation } from '@tanstack/react-query';
 
 const updateQueries = [QueryKeys.VEHICLES_PAGE_TABLE, QueryKeys.CAR_LIST, QueryKeys.CAR_ITEM];
-
-const messageCode: { [key: number]: string } = {
-  [StatusCode.CONFLICT]: 'ТС уже зарегистрировано в системе. Обратитесь к администратору',
-};
-
-const onError = (error: AxiosError<IError>) => {
-  const status = error?.response?.data?.status || error?.response?.status;
-  const message = messageCode[status];
-  if (message) {
-    enqueueSnackbar({ message, variant: 'error' });
-  }
-};
 
 export const useCarAddChangeFormApi = (id?: ID) => {
   const update = useUpdateQueries();
@@ -35,24 +23,31 @@ export const useCarAddChangeFormApi = (id?: ID) => {
 
   const { mutateAsync: changeItem } = useMutation({
     mutationFn: async (changeData: ChangeCarBody) => {
-      try {
-        const response = await CarsApi.changeCar(changeData, id);
-        if (response.status >= 400) {
-          throw new AxiosError(
-            `Request failed with status code ${response.status}`,
-            undefined,
-            response.config,
-            response.request,
-            response,
-          );
-        }
-        return response;
-      } catch (e) {
-        if (e instanceof AxiosError) {
-          onError(e);
-        }
-        throw e;
+      const response = await CarsApi.changeCar(changeData, id);
+
+      if (response.status === StatusCode.CONFLICT) {
+        const message = response.detail;
+        enqueueSnackbar({ message, variant: 'error' });
+        throw new AxiosError(
+          `Request failed with status code ${response.status}`,
+          undefined,
+          response.config,
+          response.request,
+          response,
+        );
       }
+
+      if (response.status >= 400) {
+        throw new AxiosError(
+          `Request failed with status code ${response.status}`,
+          undefined,
+          response.config,
+          response.request,
+          response,
+        );
+      }
+
+      return response;
     },
     onSuccess: () => {
       update(updateQueries);
@@ -61,24 +56,31 @@ export const useCarAddChangeFormApi = (id?: ID) => {
 
   const { mutateAsync: createItem } = useMutation({
     mutationFn: async (changeData: CreateCarBody) => {
-      try {
-        const response = await CarsApi.createCar(changeData);
-        if (response.status >= 400) {
-          throw new AxiosError(
-            `Request failed with status code ${response.status}`,
-            undefined,
-            response.config,
-            response.request,
-            response,
-          );
-        }
-        return response;
-      } catch (e) {
-        if (e instanceof AxiosError) {
-          onError(e);
-        }
-        throw e;
+      const response = await CarsApi.createCar(changeData);
+
+      if (response.status === StatusCode.CONFLICT) {
+        const message = response.detail;
+        enqueueSnackbar({ message, variant: 'error' });
+        throw new AxiosError(
+          `Request failed with status code ${response.status}`,
+          undefined,
+          response.config,
+          response.request,
+          response,
+        );
       }
+
+      if (response.status === StatusCode.CONFLICT) {
+        throw new AxiosError(
+          `Request failed with status code ${response.status}`,
+          undefined,
+          response.config,
+          response.request,
+          response,
+        );
+      }
+
+      return response;
     },
     onSuccess: () => {
       update(updateQueries);
