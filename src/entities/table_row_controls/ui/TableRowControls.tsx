47fc6be9a8a013bbc33d4a337b06a1e4a1f55b14
Evgeniy-makdak <makdak23@mail.ru>
2024-07-05 16:20:42 +0300

    В файле src/entities/table_row_controls/ui/TableRowControls.tsx начал реализовывать скрытие иконок Редактировать и Удалить для Суперадмина. Задача 288

--- src/entities/table_row_controls/ui/TableRowControls.tsx
+++ src/entities/table_row_controls/ui/TableRowControls.tsx
@@ -13,6 +13,7 @@ type TableRowControlsProps = {
   onClickEdit?: () => void;
   testidEdit?: string;
   testidDelete?: string;
+  userRole?: number;
 };
 
 export const TableRowControls: FC<TableRowControlsProps> = ({
@@ -45,3 +46,55 @@ export const TableRowControls: FC<TableRowControlsProps> = ({
     </div>
   );
 };
+
+// import type { FC } from 'react';
+
+// import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
+// import DeleteIcon from '@mui/icons-material/Delete';
+// import ModeEditIcon from '@mui/icons-material/ModeEdit';
+// import { GridActionsCellItem } from '@mui/x-data-grid';
+
+// import style from './TableRowControls.module.scss';
+
+// type TableRowControlsProps = {
+//   arrowIcon?: boolean;
+//   onClickDelete?: () => void;
+//   onClickEdit?: () => void;
+//   testidEdit?: string;
+//   testidDelete?: string;
+//   userRole?: number;
+// };
+
+// export const TableRowControls: FC<TableRowControlsProps> = ({
+//   onClickDelete,
+//   onClickEdit,
+//   testidDelete,
+//   testidEdit,
+//   arrowIcon = false,
+//   userRole = 0,
+// }) => {
+//   const isUserSuperAdmin = userRole === 100;
+
+//   return (
+//     <div className={style.controls}>
+//       {!!onClickEdit && isUserSuperAdmin && (
+//         <GridActionsCellItem
+//           data-testid={testidEdit}
+//           label="edit"
+//           icon={arrowIcon ? <ArrowForwardOutlinedIcon /> : <ModeEditIcon />}
+//           key={'add'}
+//           onClick={onClickEdit}
+//         />
+//       )}
+//       {!!onClickDelete && isUserSuperAdmin && (
+//         <GridActionsCellItem
+//           onClick={onClickDelete}
+//           key={'delete'}
+//           data-testid={testidDelete}
+//           icon={<DeleteIcon />}
+//           label="Delete"
+//         />
+//       )}
+//     </div>
+//   );
+// };
