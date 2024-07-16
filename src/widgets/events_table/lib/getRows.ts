a3f237d4ba3ca0358f1aa84bc66a2b132d9f4540
Evgeniy-makdak <makdak23@mail.ru>
2024-07-16 12:47:50 +0300

    Решена проблема с выпадающим списком событий при фильтрации по имени пользователя 504.

--- src/widgets/events_table/lib/getRows.ts
+++ src/widgets/events_table/lib/getRows.ts
@@ -1,12 +1,10 @@
 import { useMemo } from 'react';
 
-import { useStore } from 'zustand';
-
+// import { useStore } from 'zustand'; Оставил на всякий случай, если придётся запрашивать с менеджера состояний. Пока не используем.
 // import { getLastEvent } from '@entities/type_event_select';
-
 import type { GridRowsProp } from '@mui/x-data-grid';
 
-import { eventsFilterPanelStore } from '@features/events_filter_panel/model/eventsFilterPanelStore';
+// import { eventsFilterPanelStore } from '@features/events_filter_panel/model/eventsFilterPanelStore'; Также пока стор не используем.
 import { type IDeviceAction } from '@shared/types/BaseQueryTypes';
 import { Formatters } from '@shared/utils/formatters';
 
@@ -14,7 +12,7 @@ import { ValuesHeader } from './getColumns';
 
 export const useGetRows = (data: IDeviceAction[]): GridRowsProp => {
   // Получение selectedLabel из Zustand Store
-  const selectedLabel = useStore(eventsFilterPanelStore, (state) => state.selectedLabel);
+  // const selectedLabel = useStore(eventsFilterPanelStore, (state) => state.selectedLabel);
 
   const mapData = useMemo(() => {
     return (Array.isArray(data) ? data : []).map((item) => {
@@ -32,11 +30,11 @@ export const useGetRows = (data: IDeviceAction[]): GridRowsProp => {
           ? Formatters.carNameFormatter(item.vehicleRecord, true)
           : '-',
         [ValuesHeader.GOS_NUMBER]: item.vehicleRecord?.registrationNumber ?? '-',
-        [ValuesHeader.TYPE_OF_EVENT]: selectedLabel ?? item.events[0].eventType, // Использование selectedLabel
+        [ValuesHeader.TYPE_OF_EVENT]: item.events[3]?.eventType ?? item.events[0].eventType, // Использование selectedLabel при использовании стора. Пока отключено.
         // [ValuesHeader.TYPE_OF_EVENT]: lastEvent,
       };
     });
-  }, [data, selectedLabel]);
+  }, [data]);
 
   return mapData;
 };
