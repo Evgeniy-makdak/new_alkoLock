0a62cda022e457c46041d4174f2ac461d7d96c97
Evgeniy-makdak <makdak23@mail.ru>
2024-08-29 11:33:16 +0300

    Добавил хук для api. Пока не рабочий.

---
+++ src/pages/authorization/api/useChangePasswordApi.ts
@@ -0,0 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+
+import type { AppAxiosResponse } from '@shared/api/baseQueryTypes';
+import { UsersApi } from '@shared/api/baseQuerys';
+import { RoutePaths } from '@shared/config/routePathsEnum';
+import type { ChangePasswordData } from '@shared/types/BaseQueryTypes';
+import { useMutation } from '@tanstack/react-query';
+
+export const useChangePasswordApi = (onSuccess?: (data: AppAxiosResponse<unknown>) => void) => {
+  const navigate = useNavigate();
+
+  const { isPending, mutate, isError, isSuccess, data } = useMutation({
+    mutationFn: (data: ChangePasswordData) => UsersApi.changePassword(data),
+    onSuccess: (data) => {
+      if (onSuccess) {
+        onSuccess(data);
+      }
+      navigate(RoutePaths.auth);
+    },
+  });
+console.log(data);
+
+  return {
+    mutate,
+    isLoading: isPending,
+    isError,
+    isSuccess,
+    data,
+  };
+};
