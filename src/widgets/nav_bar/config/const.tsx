d68badf235bb73d2337f0e6050027dd45439587d
Evgeniy-makdak <makdak23@mail.ru>
2024-09-09 14:54:15 +0300

    Отловил событие Начало тестирования -> Тестирование. Изменил версию фронта

--- src/widgets/nav_bar/config/const.tsx
+++ src/widgets/nav_bar/config/const.tsx
@@ -10,7 +10,7 @@ import TimeToLeaveOutlinedIcon from '@mui/icons-material/TimeToLeaveOutlined';
 import { RoutePaths } from '@shared/config/routePathsEnum';
 import { ExtractTypeFromArray } from '@shared/types/utility';
 
-export const frontendVersion = '1.16.4';
+export const frontendVersion = '1.16.5';
 
 export const NAV_LINKS = [
   {
