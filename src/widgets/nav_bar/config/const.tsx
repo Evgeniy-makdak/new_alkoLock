4e56cfff1cbad990347eba9b6586076f014b797b
Evgeniy-makdak <makdak23@mail.ru>
2024-11-18 15:18:13 +0300

    1.22.2 Поменял версию фронта

--- src/widgets/nav_bar/config/const.tsx
+++ src/widgets/nav_bar/config/const.tsx
@@ -10,7 +10,7 @@ import TimeToLeaveOutlinedIcon from '@mui/icons-material/TimeToLeaveOutlined';
 import { RoutePaths } from '@shared/config/routePathsEnum';
 import { ExtractTypeFromArray } from '@shared/types/utility';
 
-export const frontendVersion = '1.22.1';
+export const frontendVersion = '1.22.2';
 
 export const NAV_LINKS = [
   {
